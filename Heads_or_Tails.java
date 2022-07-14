535b3738a82146d98c80c629b6c4ea9c82886acd
momopon1415 <fortnitet-hyaku@outlook.jp>
2022-07-14 15:50:29 +0900

    User name added

--- Heads_or_Tails.java
+++ Heads_or_Tails.java
@@ -1,10 +1,15 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Heads_or_Tails {
     public static void main(String[] args) {
         Random random = new Random();
         int Heads = 0;
         int Tails = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Who are you?");
+        String username = scanner.next();
+        System.out.println("Hello, " + username + "!");
         System.out.println("Tossing a coin...");
         for (int i = 1; i < 4; i++) {
             int randomValue = random.nextInt(2);
